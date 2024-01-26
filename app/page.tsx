c85a14199bcb8a10eea252bdf062e5af5eefe71c
Varun Vasudeva <varunvasudeva1@gmail.com>
2024-01-25 21:22:38 -0600

    Remove unused code and imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 import Card from "@/components/Card";
 import { TbSparkles } from "react-icons/tb";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
 import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
 export default async function Index() {
-  const supabase = createServerComponentClient({ cookies });
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   return (
     <div className="flex flex-col items-center overflow-clip bg-black">
       <section className="flex flex-col items-center justify-center w-screen h-screen bg-gradient-to-b from-black via-purple-950 to-black bg-blur-lg px-10">
