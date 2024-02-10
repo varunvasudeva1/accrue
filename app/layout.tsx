7871ec0e903dfab44041a032743b592ca8bd4a6c
Varun Vasudeva <varunvasudeva1@gmail.com>
2024-02-09 23:53:03 -0600

    Add links to About and Privacy Policy pages in footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import Header from "@/components/Header";
 import { getCurrentUser } from "@/actions";
 import { Metadata, Viewport } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Accrue",
@@ -40,14 +41,26 @@ export default async function RootLayout({
           theme="dark"
         />
         <footer className="flex flex-row justify-between items-center w-screen bg-zinc-900 text-center p-5 font-mono">
-          <div className="flex flex-col justify-center items-start">
-            <p className="text-purple-300 font-semibold text-md">
-              © 2024 Accrue. All rights reserved.
-            </p>
-            <span className="text-gray-200 text-sm">
-              Designed with <span className="text-purple-300 font-bold">♥</span>{" "}
-              by Momenta Labs
-            </span>
+          <div className="flex flex-col sm:flex-row justify-start items-start sm:justify-between sm:items-center w-full space-y-4 sm:space-y-0">
+            <div className="flex flex-col justify-start items-start space-y-1">
+              <Link href="/about" className="text-gray-200 text-sm">
+                About
+              </Link>
+              <Link href="/privacy" className="text-gray-200 text-sm">
+                Privacy Policy
+              </Link>
+            </div>
+
+            <div className="flex flex-col items-start sm:items-end">
+              <p className="text-purple-300 font-semibold text-sm">
+                © 2024 Accrue. All rights reserved.
+              </p>
+              <span className="text-gray-200 text-xs">
+                Designed with{" "}
+                <span className="text-purple-300 font-bold">♥</span> by Momenta
+                Labs
+              </span>
+            </div>
           </div>
         </footer>
       </body>
