1532070cd8bb6a96dc2e8cdf638404a8da965002
Varun Vasudeva <varunvasudeva1@gmail.com>
2024-02-11 00:24:38 -0600

    Add privacy policy layout and page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Privacy | Accrue",
@@ -9,18 +10,24 @@ export default async function Index() {
   return (
     <div className="flex flex-col items-start justify-start w-full space-y-4">
       <h3 className="font-bold text-4xl lg:text-5xl text-purple-200 pb-2">
-        privacy policy
+        privacy
       </h3>
       <div className="flex flex-col items-start self-center max-w-3xl space-y-4">
         <p className="text-lg text-white leading-relaxed">
-          We're very committed to user privacy. This policy outlines what
-          information we collect, how we use it, and with whom we share it.
+          We don't think using a product means the company whose product it is
+          should know everything about you. We collect only the information we
+          need to provide our services, and we don't share it with anyone. Ever.
+          For any reason. We encourage you to{" "}
+          <Link href="/privacy-policy" className="text-purple-200 font-mono">
+            read our Privacy Policy
+          </Link>{" "}
+          to understand how we handle your data.
         </p>
         <div className="flex flex-col items-start bg-zinc-900 rounded-md p-6">
           <h3 className="font-bold text-xl lg:text-2xl text-purple-200 pb-2">
             summary
           </h3>
-          <ul className="list-disc list-inside text-white text-lg">
+          <ul className="list-disc list-inside text-white text-lg font-mono">
             <li>no PII collected</li>
             <li>anonymous analytics</li>
             <li>only functional cookies</li>
@@ -28,79 +35,6 @@ export default async function Index() {
             <li>no sharing data with third parties</li>
           </ul>
         </div>
-        <h4 className="font-bold text-xl lg:text-2xl text-purple-200 pb-2">
-          Information We Collect
-        </h4>
-        <p className="text-lg text-white leading-relaxed">
-          We do not collect any personally identifiable information (PII) from
-          our users. We require users to create an account with an email in
-          order to use our services. We only collect information that is
-          necessary for the operation of our platform, such as email addresses
-          and user-generated content (project data, chat messages, etc.).
-        </p>
-        <h4 className="font-bold text-xl lg:text-2xl text-purple-200 pb-2">
-          How We Use Information
-        </h4>
-        <p className="text-lg text-white leading-relaxed">
-          We use the information we collect to operate, improve, and expand our
-          services. This includes using anonymous analytics tools to understand
-          how users interact with our platform and make data-driven decisions
-          about how to improve their experience.
-        </p>
-        <h4 className="font-bold text-xl lg:text-2xl text-purple-200 pb-2">
-          With Whom We Share Information
-        </h4>
-        <p className="text-lg text-white leading-relaxed">
-          We do not share any user information with third parties. We believe in
-          protecting the privacy of our users and do not sell or share their
-          data with anyone. This includes user information any user-generated
-          content on our platform.
-        </p>
-        <h4 className="font-bold text-xl lg:text-2xl text-purple-200 pb-2">
-          Cookies and Other Tracking Technologies
-        </h4>
-        <p className="text-lg text-white leading-relaxed">
-          We use functional cookies to enable a better user experience. We do
-          not use any other tracking technologies to collect information about
-          you.
-        </p>
-        <h4 className="font-bold text-xl lg:text-2xl text-purple-200 pb-2">
-          Third Party Services
-        </h4>
-        <p className="text-lg text-white leading-relaxed">
-          We may use third party services to provide certain features or
-          functionality on our platform. These services may collect information
-          about you, but we do not share this information with them. We will
-          only use these services if they are necessary for the operation of our
-          platform and have agreed to adhere to our privacy policies.
-        </p>
-        <h4 className="font-bold text-xl lg:text-2xl text-purple-200 pb-2">
-          Changes to This Policy
-        </h4>
-        <p className="text-lg text-white leading-relaxed">
-          We reserve the right to modify this policy at any time. We will notify
-          users of any changes to this policy by updating the "last updated"
-          date at the top of this page and by sending an email to the email
-          address associated with their account. We encourage users to review
-          this policy frequently to stay informed about how we are protecting
-          their information.
-        </p>
-        <h4 className="font-bold text-xl lg:text-2xl text-purple-200 pb-2">
-          Contact Us
-        </h4>
-        <p className="text-lg text-white leading-relaxed">
-          If you have any questions or concerns about our privacy policies,
-          please contact us at
-          <a
-            href="mailto:
-          support@accrue.com"
-            className="text-purple-300"
-          >
-            {" "}
-            support@accrueapp.com
-          </a>
-          .
-        </p>
       </div>
     </div>
   );
